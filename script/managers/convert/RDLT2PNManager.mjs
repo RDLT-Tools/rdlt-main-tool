4fb907511fb77906543f5cd86b8df0741929cfff
Lyward Manuel S. Tongzon <lstongzon@up.edu.ph>
2025-05-06 13:12:12 +0800

    Integrated rdlt2pn

--- script/managers/convert/RDLT2PNManager.mjs
+++ script/managers/convert/RDLT2PNManager.mjs
@@ -33,7 +33,30 @@ export class RDLT2PNManager {
         const iframe = rootElement.querySelector("iframe");
         
         iframe.addEventListener("load", () => {
-            // TODO: Setup PN conversion here (use `simpleModel`)
+            iframe.contentWindow.renderConversion(
+                {
+                    vertices: [
+                      { id: "x1", type: "b", label: "", M: 0 },
+                      { id: "y1", type: "c", label: "", M: 0 },
+                      { id: "y2", type: "c", label: "", M: 0 },
+                      { id: "y3", type: "c", label: "", M: 0 },
+                      { id: "x2", type: "e", label: "", M: 1 },
+                      { id: "y4", type: "c", label: "", M: 0 },
+                      { id: "y5", type: "c", label: "", M: 0 }
+                    ],
+                    edges: [
+                      { from: "x1", to: "y1", C: "a", L: 1 },
+                      { from: "x1", to: "y2", C: "b", L: 1 },
+                      { from: "y2", to: "y3", C: "d", L: 1 },
+                      { from: "y1", to: "x2", C: "ϵ", L: 2 },
+                      { from: "y2", to: "x2", C: "send m", L: 2 },
+                      { from: "x2", to: "y5", C: "ϵ", L: 1 },
+                      { from: "x2", to: "y4", C: "ϵ", L: 1 },
+                      { from: "y4", to: "y5", C: "ϵ", L: 1 },
+                      { from: "y5", to: "y3", C: "send n, p", L: 1 }
+                    ]
+                }
+            ); 
         });
     }
 }
